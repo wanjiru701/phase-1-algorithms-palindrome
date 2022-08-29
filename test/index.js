0a899a05177ff7cad1a6e2b300ce4143677a0696
whitney <whitney.njogu@student.moringaschool.com>
2022-08-29 22:05:25 +0300

    Completed assignment

--- test/index.js
+++ test/index.js
@@ -1,21 +1,9 @@
-const chai = require("chai");
-global.expect = chai.expect;
-const isPalindrome = require("../index");
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
 
-describe("isPalindrome", () => {
-  it("returns true for 'abba'", () => {
-    expect(isPalindrome("abba")).to.be.true;
-  });
-  it("returns true for 'racecar'", () => {
-    expect(isPalindrome("racecar")).to.be.true;
-  });
-  it("returns true for 'a'", () => {
-    expect(isPalindrome("a")).to.be.true;
-  });
-  it("returns false for 'robot'", () => {
-    expect(isPalindrome("robot")).to.be.false;
-  });
-  it("returns false for 'ab'", () => {
-    expect(isPalindrome("ab")).to.be.false;
-  });
-});
+function isPalindrome(word) {
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
+}
